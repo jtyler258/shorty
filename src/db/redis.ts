41cce69aecf909e675b5af81495165e11a0d90a8
John Tyler <john.tyler@fsgi.com>
2020-01-01 13:02:01 -0600

    forgot to add TTL setting

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -1,13 +1,15 @@
 import { Tedis } from 'tedis'
-import { REDIS_URL } from '../utils/config';
+import { REDIS_URL, REDIS_TTL } from '../utils/config';
 
 const tedis = new Tedis({
   host: REDIS_URL
 });
 
+const TTL = Number.parseInt(REDIS_TTL);
+
 export default class Redis {
-  static async set(key: string, value: string, ttl: number = 604800): Promise<void> {
-    return await tedis.setex(key, ttl, value)
+  static async set(key: string, value: string): Promise<void> {
+    return await tedis.setex(key, TTL, value)
   }
 
   static async get(key: string): Promise<string|number|null> {
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,3 +2,4 @@ export const HOSTED_DOMAIN = process.env.DOMAIN || "localhost";
 export const HOSTED_PORT = process.env.HOSTED_PORT || "";
 export const HOSTED_PROTOCOL = process.env.HOSTED_PROTOCOL || "http";
 export const REDIS_URL = process.env.REDIS_URL || "localhost";
+export const REDIS_TTL = process.env.REDIS_TTL || "604800";
